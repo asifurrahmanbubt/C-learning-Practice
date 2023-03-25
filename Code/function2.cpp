de4ce49a90a16a1b45965d185ff9a87d9381e482
Asifur Rahman <122219223+asifurrahmanbubt@users.noreply.github.com>
2023-03-25 19:40:20 +0600

    Add files via upload

---
+++ Code/function2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+void addNumber(){
+    int num1= 5;
+    int num2= 6;
+    int result;
+    result= num1+num2;
+    cout<<"Addition is : " << result << endl;
+}
+int main(){
+    addNumber();
+
+return 0;
+}

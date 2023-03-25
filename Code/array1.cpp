ec1718eed7666fb22abff870a0be407f35e24dd3
Asifur Rahman <122219223+asifurrahmanbubt@users.noreply.github.com>
2023-03-25 21:46:09 +0600

    Add files via upload

---
+++ Code/array1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int age[10]={23,21,22,20,19,27,19,87,9,99};
+    cout<< age[0]<<endl;
+    cout<< age[1] << endl;
+    cout << age[2] << endl;
+    cout << age[3] << endl;
+    cout << age[5] << endl;
+
+return 0;
+}

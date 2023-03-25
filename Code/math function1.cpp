e2984ba371d617ea934af685e0061f615343d889
Asifur Rahman <122219223+asifurrahmanbubt@users.noreply.github.com>
2023-03-25 21:45:41 +0600

    Add files via upload

---
+++ Code/math function1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+    double num1,num2,num3;
+    num1= 150;
+    num2= 34.04;
+    num3= 2;
+
+    cout<< "The Sine is : " << sin(num1) << endl;
+    cout<< "The Power is : " << pow(num2,num3) << endl;
+    cout<< "The Root is : " << sqrt(num3) <<  endl;
+    cout<< "The Ceiling Value is "  << ceil(num2) << endl;
+    cout<< "The Floor Value is : " << floor(num2) << endl;
+    cout << "The Absolute Value is : " << fabs(num2) << endl;
+
+return 0;
+
+}

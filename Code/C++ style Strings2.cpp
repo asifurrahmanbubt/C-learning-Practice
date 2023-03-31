7a9bd472d0b45fc0ca0701119b907a16e4ce32f1
Asifur Rahman <122219223+asifurrahmanbubt@users.noreply.github.com>
2023-03-31 16:25:22 +0600

    Add files via upload

---
+++ Code/C++ style Strings2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+
+    string name;
+    cout<< "Enter Your Name :";
+    cin>>name;
+    cout<< "Hello" << name << endl;
+
+return 0;
+}

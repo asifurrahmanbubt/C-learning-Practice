c88f82672263a7b3a379fb9a286f9832926c0c01
Asifur Rahman <122219223+asifurrahmanbubt@users.noreply.github.com>
2023-03-31 16:25:02 +0600

    Add files via upload

---
+++ Code/C++ style Strings1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    string user = "John";
+    string website = "GitHub";
+    cout << user << endl;
+    cout << website << endl;
+
+return 0;
+}

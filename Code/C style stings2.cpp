bc4c8c933283b107bccb1d82ae779c36dc544b8b
Asifur Rahman <122219223+asifurrahmanbubt@users.noreply.github.com>
2023-03-31 16:24:32 +0600

    Add files via upload

---
+++ Code/C style stings2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    char name[10];
+
+    cout << "Enter your Name: ";
+    cin>> name;
+    cout<< "Your Name is : " << name << endl;
+
+return 0;
+}

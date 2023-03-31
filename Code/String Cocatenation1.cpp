eb33f28ae89c6103901cf86c19c105165c02a58a
Asifur Rahman <122219223+asifurrahmanbubt@users.noreply.github.com>
2023-03-31 16:25:49 +0600

    Add files via upload

---
+++ Code/String Cocatenation1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    string firstname = "Git";
+    string lastname = "Hub";
+    string fullname = firstname + lastname;
+    cout<< "Full Name is : " << fullname;
+
+return 0;
+
+}
